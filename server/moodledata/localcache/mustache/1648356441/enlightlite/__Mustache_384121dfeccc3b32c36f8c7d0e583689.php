d6af25f5bd7431bc1e81dc47484e186bc9505f26
kevinfaccini <kevinfaccini@gmail.com>
2022-03-29 19:41:30 -0500

    Creating my first website

---
+++ server/moodledata/localcache/mustache/1648356441/enlightlite/__Mustache_384121dfeccc3b32c36f8c7d0e583689.php
@@ -0,0 +1,83 @@
+<?php
+
+class __Mustache_384121dfeccc3b32c36f8c7d0e583689 extends Mustache_Template
+{
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $buffer = '';
+
+        if ($partial = $this->mustache->loadPartial('theme_boost/head')) {
+            $buffer .= $partial->renderInternal($context);
+        }
+        $buffer .= $indent . '
+';
+        $buffer .= $indent . '<body ';
+        $value = $this->resolveValue($context->find('bodyattributes'), $context);
+        $buffer .= $value;
+        $buffer .= '>
+';
+        $buffer .= $indent . '
+';
+        $buffer .= $indent . '<div id="page-wrapper">
+';
+        $buffer .= $indent . '
+';
+        $buffer .= $indent . '    ';
+        $value = $this->resolveValue($context->findDot('output.standard_top_of_body_html'), $context);
+        $buffer .= $value;
+        $buffer .= '
+';
+        $buffer .= $indent . '
+';
+        if ($partial = $this->mustache->loadPartial('theme_enlightlite/header')) {
+            $buffer .= $partial->renderInternal($context, $indent . '    ');
+        }
+        $buffer .= $indent . '
+';
+        $buffer .= $indent . '    <div id="page" class="container-fluid mt-0">
+';
+        $buffer .= $indent . '        <div id="page-content" class="row">
+';
+        $buffer .= $indent . '            <div id="region-main-box" class="col-12">
+';
+        $buffer .= $indent . '                <section id="region-main" class="col-12">
+';
+        $buffer .= $indent . '                    ';
+        $value = $this->resolveValue($context->findDot('output.course_content_header'), $context);
+        $buffer .= $value;
+        $buffer .= '
+';
+        $buffer .= $indent . '                    ';
+        $value = $this->resolveValue($context->findDot('output.main_content'), $context);
+        $buffer .= $value;
+        $buffer .= '
+';
+        $buffer .= $indent . '                    ';
+        $value = $this->resolveValue($context->findDot('output.course_content_footer'), $context);
+        $buffer .= $value;
+        $buffer .= '
+';
+        $buffer .= $indent . '                </section>
+';
+        $buffer .= $indent . '            </div>
+';
+        $buffer .= $indent . '        </div>
+';
+        $buffer .= $indent . '    </div>
+';
+        $buffer .= $indent . '</div>
+';
+        if ($partial = $this->mustache->loadPartial('theme_boost/nav-drawer')) {
+            $buffer .= $partial->renderInternal($context);
+        }
+        if ($partial = $this->mustache->loadPartial('theme_enlightlite/footer')) {
+            $buffer .= $partial->renderInternal($context);
+        }
+        $buffer .= $indent . '
+';
+        $buffer .= $indent . '
+';
+
+        return $buffer;
+    }
+}
